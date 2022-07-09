5c54fd234260f6d8b23986be8f3a70d30303e1cc
Kamo <ngcd123@naver.com>
2022-07-09 22:53:26 +0900

    마지막 결과값 구현

--- js/start.js
+++ js/start.js
@@ -1,8 +1,11 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
+const button = document.querySelector("#button");
+const select = [];
 
 
-function addAnswer(answerText, number) {
+
+function addAnswer(answerText, number,i) {
     const a = document.querySelector(".answerBox");
     const answer = document.createElement('button');
     answer.classList.add('answerList','btn','btn-light');
@@ -10,6 +13,9 @@ function addAnswer(answerText, number) {
     answer.innerHTML = answerText;
 
     answer.addEventListener("click", function () {
+        select.unshift(i);
+        console.log(select);
+        
         var children = document.querySelectorAll(".answerList");
         for(let  i = 0; i< children.length; i++) {
             children[i].disabled = true;
@@ -20,12 +26,106 @@ function addAnswer(answerText, number) {
     },false);
 }
 function goNext(number) {
-    const q = document.querySelector(".qBox");
-    q.innerHTML = qnaList[number].q;
-    for(let i in qnaList[number].a) {
-        addAnswer(qnaList[number].a[i].answer, number);
+        if(number ==10) {
+            qna.style.display ='none'; 
+            let _0number = 0;
+            let _1number = 0;
+             for(let i in select) {
+                
+                 if(select.at(i) =="0") {
+                     _0number = _0number+1;
+                    
+                 } else if (select.at(i) =="1") {
+                     _1number = _1number +1;
+                 } 
+             }
+             main.style.animation = 'none';
+             main.innerHTML = 'Test Over!';
+             main.style.display = 'block';
+             qna.style.display = 'none';    
+
+            const result = document.createElement("h1");
+            
+            const String1 =  _0number;
+            String1.toString();
+            if(String1 =='0') {
+                result.innerText ="1번지문의 확률이 "+String1+"%입니다.";
+            }else {
+                result.innerText ="1번지문의 확률이 "+String1+"0%입니다.";
+            }
+
+            main.appendChild(result);
+
+          
+          
+             
+             
+             
+            //  main.style.display = 'block';
+
+            //  switch(_0number) {
+            //       case 0:  // if (x === 'value1')
+            //       console.log("0지   문의 확률 0%");
+            //       break;
+              
+            //       case 1:  // if (x === 'value1')
+            //       console.log("0지문의 확률 10%");
+            //       break;
+                  
+            //       case 2:  // if (x === 'value1')
+            //       console.log("0지문의 확률 20%");
+            //       break;
+                  
+            //       case 3:  // if (x === 'value1')
+            //       console.log("0지문의 확률 30%");
+            //       break;
+                  
+            //       case 4:  // if (x === 'value1')
+            //       console.log("0지문의 확률 40%");
+            //       break;
+                  
+            //       case 5:  // if (x === 'value1')
+            //       console.log("0지문의 확률 50%");
+            //       break;
+                  
+            //       case 6:  // if (x === 'value1')
+            //       console.log("0지문의 확률 60%");
+            //       break;
+                  
+            //       case 7:  // if (x === 'value1')
+            //       console.log("0지문의 확률 70%");
+            //       break;
+                  
+            //       case 8:  // if (x === 'value1')
+            //       console.log("0지문의 확률 80%");
+            //       break;
+                  
+            //       case 9:  // if (x === 'value1')
+            //       console.log("0지문의 확률 90%");
+            //       break;
+                  
+            //       case 10:  // if (x === 'value1')
+            //       console.log("0지문의 확률 100%");
+            //     //   break;    
+                  
+            //   }
+             console.log("0의개수:", _0number);
+             console.log("1의개수:", _1number);
+             
+            
+            
+            return;
+        }
+
+
+        const q = document.querySelector(".qBox");
+        q.innerHTML = qnaList[number].q;
+        for(let i in qnaList[number].a) {
+            addAnswer(qnaList[number].a[i].answer, number,i);
+        }
     }
-}
+    
+
 
 function begin() {
     
